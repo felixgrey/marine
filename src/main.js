945643edcc07b2c300525c8f9de7fe3fd8977eac
guo.feng <guo.feng@neusoft.com>
2019-04-03 05:07:35 +0800

    restart

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
 
 Vue.use(VueRouter)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
