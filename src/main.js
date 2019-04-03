28bb8d2670b52ba54987be757f2e7500474bc8fb
guo.feng <guo.feng@neusoft.com>
2019-04-03 10:10:07 +0800

    ui

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
-import '@/components/common/marine/marine-vue'
+import '@/components/common/marine-167/marine-vue'
 
 Vue.use(VueRouter)
 Vue.use(ElementUI)
